261e557d4c81205b5daad321bd666f2ac5464faa
Andrew <andrew.li729@gmail.com>
2020-12-09 00:25:33 -0600

    created ItemInInventory class

---
+++ main.py
@@ -0,0 +1,13 @@
+# Andrew Li
+# 1824794
+import csv
+
+
+class ItemInInventory:
+    def __init__(self, item_id="none", item_man_name="none", price=0, service_date="none", item_damage="none"):
+        self.item_id = item_id
+        self.item_man_name = item_man_name
+        self.price = price
+        self.service_date = service_date
+        self.item_damage = item_damage
+
